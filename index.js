c3e1d22e7b7f437725cc1060bdf0280f34bbb190
Andrew Kloecker <lt.kvothe@gmail.com>
2020-07-07 08:18:57 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+const greet = (stringTime) => {
+  const number = parseInt(stringTime.split(":")[0]);
+  let ans = "";
+  if(number > 0 && number < 12){
+    ans = 'Morning';
+  }else if(number >= 12 && number < 19){
+    ans = 'Afternoon';
+  } else {
+    ans = 'Evening';
+  }
+  return 'Good ' + ans;
+}
 /* Write your implementation of displayMessage() */
+const displayMessage = (string) => document.getElementById("greeting").innerText = string;
